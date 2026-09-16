2b0267b3c9a7a4c9c42fe8c6f19ceec65759af26
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-16 13:05:52 +0530

    fix: add public library entrypoint

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './types.js';
+export * from './scenarios.js';
+export * from './runners/adapters.js';
+export * from './runners/runner.js';
+export * from './utils/scoring.js';
+export {parseConfig} from './utils/config.js';
