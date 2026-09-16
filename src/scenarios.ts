68a8760b27568c09f96f62dca72778f22a53fcbc
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-16 13:21:21 +0530

    feat: expose exact upstream scenario loader

--- src/scenarios.ts
+++ src/scenarios.ts
@@ -1,1 +1,2 @@
-export {BENCHMARK_VERSION, SCENARIOS, allScenarios, loadScenarios} from './scenario-pack.js';
+export {BENCHMARK_VERSION,SCENARIOS,allScenarios,loadScenarios} from './scenario-pack.js';
+export {UPSTREAM_SOURCE,convertUpstreamScenario,loadUpstreamScenarios,loadUpstreamFromManifest} from './scenario-loader.js';
